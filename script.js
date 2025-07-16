6bd1a56e9460fb4b88f28b89d52f2fc33cfb453e
tnqls3128 <tnqls3128@naver.com>
2025-07-16 12:00:39 +0900

    250716

--- script.js
+++ script.js
@@ -5,11 +5,66 @@ fetch(url)
   .then((response) => response.json())
   .then((result) => {
     const data = result.response.body.items.item;
-
+    console.log(data);
     const showPosition = (position) => {
       const { latitude, longitude } = position.coords;
-
+      console.log(latitude, longitude);
       const container = document.getElementById("map");
+
+      var options = {
+        //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(latitude, longitude), //지도의 중심좌표.
+        level: 3, //지도의 레벨(확대, 축소 정도)
+      };
+
+      var map = new kakao.maps.Map(container, options);
+
+      var clusterer = new kakao.maps.MarkerClusterer({
+        map: map,
+        averageCenter: true,
+        minLevel: 10,
+      });
+
+      let markers = [];
+
+      for (var i = 0; i < data.length; i++) {
+        var marker = new kakao.maps.Marker({
+          map: map,
+          position: new kakao.maps.LatLng(data[i].mapY, data[i].mapX),
+        });
+
+        markers.push(marker);
+
+        var infowindow = new kakao.maps.InfoWindow({
+          content: data[i].facltNm,
+        });
+
+        function makeOverListener(map, marker, infowindow) {
+          return function () {
+            infowindow.open(map, marker);
+          };
+        }
+
+        function makeOutListener(infowindow) {
+          return function () {
+            infowindow.close();
+          };
+        }
+
+        kakao.maps.event.addListener(
+          marker,
+          "mouseover",
+          makeOverListener(map, marker, infowindow)
+        );
+
+        kakao.maps.event.addListener(
+          marker,
+          "mouseout",
+          makeOutListener(infowindow)
+        );
+      }
+
+      clusterer.addMarkers(markers);
     };
 
     const errorPosition = (error) => {
